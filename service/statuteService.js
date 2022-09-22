bb60625634c2fb983ee69e315b2ae413ba7421d7
JUNGMINGYO <jywlsh3@naver.com>
2022-09-22 18:29:35 +0900

    xml parsing

--- service/statuteService.js
+++ service/statuteService.js
@@ -1,11 +1,28 @@
 import Statute from '../database/mysql/models/statute.js';
 import request from 'request';
+import cheerio from 'cheerio';
+import axios from 'axios';
+import convert from 'xml-js';
+import errors from '../models/core/errors.js';
 
 const getTestLaw = async () => {
-	const testUrl = 'http://www.law.go.kr/DRF/lawSearch.do?OC=jywlsh3&target=oldAndNew&type=HTML';
-	// const testUrl = 'http://www.law.go.kr/DRF/lawSearch.do?OC=jywlsh3&target=oldAndNew&type=HTML&query='+encodeURI('자동차관리법');	
-	const response = await request(testUrl);
-	return response;
+	try{
+		let testUrl = 'http://www.law.go.kr/DRF/lawSearch.do?OC=jywlsh3&target=oldAndNew&type=XML';
+		testUrl += '&query='+encodeURI('자동차관리법');
+		const response = await axios.get(testUrl);		
+		const resBody = response.data;
+		const xmlToJson = JSON.parse(convert.xml2json(resBody, {compact: true, spaces: 4}));
+		console.log(typeof xmlToJson);
+		if(xmlToJson.OldAndNewLawSearch.oldAndNew.length > 0) {
+			return xmlToJson.OldAndNewLawSearch.oldAndNew;
+		} else {
+			return false;
+		}
+		
+	} catch(e) {
+		throw new errors.InvalidDataError(e.message);
+	}
+	
 };
 
 // const createReview = async (review) => {
